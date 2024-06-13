a36b2528083262af3ba8773e0977c2829a747472
Viacheslav Klinov <77786494+VKlinov08@users.noreply.github.com>
2024-06-13 11:44:11 +0300

    Update main.py
    Added hints for using experiment files

--- Ensemble Kohonen Classifier/src/main.py
+++ Ensemble Kohonen Classifier/src/main.py
@@ -1,6 +1,6 @@
 from models.utils import image_loading
 from demo import *
-from experiments import experiment_4, experiment_5
+from experiments import experiment_2, experiment_3, experiment_4, experiment_5
 
 DEFAULT_IMAGES_PATH = r'..\resources\coins'
 DEFAULT_CLASS_NAMES_PATH = r'..\resources\coins\classnames.txt'
@@ -8,12 +8,12 @@ DEFAULT_CLASS_NAMES_PATH = r'..\resources\coins\classnames.txt'
 
 def main():
     training_images, class_names = image_loading.load_training_images(DEFAULT_IMAGES_PATH, DEFAULT_CLASS_NAMES_PATH)
-    # demo(training_images, class_names, test_classification=False)
+    demo(training_images, class_names, test_classification=False)
+    # experiment_2.run(training_images)
+    # experiment_3.run(classifier, training_images)
     # experiment_4.run(training_images)
-    experiment_5.run(training_images)
-    # test_2.run(training_images)
-    # test_3.run(classifier, training_images)
-
+    # experiment_5.run(training_images)
+    
 
 if __name__ == '__main__':
     main()
